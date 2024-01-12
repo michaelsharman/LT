70161f51efbc82a7918d2e93d4c8169527da923f
Michael Sharman <sharmo@gmail.com>
2024-01-12 19:20:34 +1100

    Now accepts question references instead of index on item

--- src/assessment/questions.js
+++ src/assessment/questions.js
@@ -9,38 +9,45 @@ import * as logger from '../utils/logger';
  */
 
 /**
- * Returns the question JSON of the current item.
+ * Returns the question JSON on the current item.
  *
- * If the item is multi-part, pass `index` to return
- * the desired question.
+ * If the item is multi-part, pass `response_id` to return
+ * the desired question. This argument will also look across
+ * all items for the requested `response_id`.
  *
  * Returns {} if no question was found on the current item.
  * @since 0.1.0
- * @param {number=} index Which question to retrieve. Numbered
- * from 1, top to bottom, left to right.
+ * @param {string=} response_id
  * @returns {object} A question JSON object.
  */
-export function question(index = 1) {
-    const item = items.item();
-    if (index <= item.questions.length) {
-        return item.questions[index - 1];
+export function question(response_id) {
+    let id = response_id ? response_id : questionResponseIds()[0];
+
+    if (id) {
+        return app.appInstance().question(id).getQuestion();
     } else {
-        logger.error(`Question not found (index ${index})`);
+        logger.error(`Question not found (index ${id})`);
+        return {};
     }
 }
 
 /**
  * Returns the question app instance on the current item.
  *
- * If the item is multi-part, pass `index` to return the
+ * If the item is multi-part, pass `response_id` to return the
  * desired question.
  * @since 0.4.0
- * @param {number=} index Which question to retrieve. Numbered
- * from 1, top to bottom, left to right.
+ * @param {string=} response_id
  * @returns {object} A question app instance.
  */
-export function questionInstance(index = 1) {
-    return app.appInstance().question(question(index).response_id);
+export function questionInstance(response_id) {
+    let id = response_id ? response_id : questionResponseIds()[0];
+
+    if (id) {
+        return app.appInstance().question(id);
+    } else {
+        return {};
+    }
 }
 
 /**
@@ -58,19 +65,21 @@ export function questions() {
  * A response object for a question on the
  * current item. Defaults to the first question.
  *
- * Pass `index` if you want a different question
+ * Pass `response_id` if you want a different question
  * response returned in the case of a multi-part item.
  *
  * Returns {} if no questions found on the item.
  * @since 0.1.0
- * @param {number=} index - Which question response to retrieve.
- * Numbered from 1, top to bottom, left to right.
+ * @param {string=} response_id
  * @returns {object} The response object for the question, null if no attempts yet.
  */
-export function questionResponse(index = 1) {
-    const q = question(index);
-    if (q !== undefined) {
-        return response(q.response_id);
+export function questionResponse(response_id) {
+    let id = response_id ? response_id : questionResponseIds()[0];
+
+    if (id) {
+        return response(id);
+    } else {
+        return {};
     }
 }
 
@@ -87,27 +96,26 @@ export function questionResponseIds() {
  * The score object for a question on the current item.
  * Defaults to the first question.
  *
- * Pass `index` if you want a different question response
- * returned in the case of a multi-part item.
+ * Pass `response_id` if you want a different question
+ * response returned in the case of a multi-part item.
  *
  * Returns {} if no questions found on the item.
  * @since 0.1.0
- * @param {number=} index - Which question response to retrieve.
- * Numbered from 1, top to bottom, left to right.
+ * @param {string=} response_id
  * @returns {object} The score object for the question, null if no attempts yet
  */
-export function questionScore(index = 1) {
-    const q = question(index);
-    if (q !== undefined) {
-        return app.appInstance().getScores()[q.response_id];
+export function questionScore(response_id) {
+    let id = response_id ? response_id : questionResponseIds()[0];
+
+    if (id) {
+        return app.appInstance().getScores()[id];
+    } else {
+        return {};
     }
 }
 
 /**
  * A response object for a single question on the current item.
- *
- *  This is generally used internally. You should instead use
- * `questionResponse()`.
  *
  * Returns {} if a response isn't found.
  * @since 0.1.0
@@ -115,7 +123,7 @@ export function questionScore(index = 1) {
  * @returns {object | null} The response object for the question
  * @ignore
  */
-export function response(response_id) {
+function response(response_id) {
     const r = app.appInstance().question(response_id);
 
     if (r) {
