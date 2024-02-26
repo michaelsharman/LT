4fdb2d7346ee6ed7cfb5c418ddd71238cb7902e9
Michael Sharman <sharmo@gmail.com>
2024-02-26 15:28:58 +1100

    Added method to determine which widget type is being edited.

---
+++ src/authoring/core/widgets.js
@@ -0,0 +1,16 @@
+import * as app from './app';
+
+/**
+ * Everything relating to a widget being created/edited.
+ * @module Authoring/Widgets
+ */
+
+/**
+ * The `type` of widget currently being edited. Returns
+ * `undefined` if not on the widget edit screen.
+ * @since 2.4.0
+ * @returns {string | undefined}
+ */
+export function type() {
+    return app.appInstance().getWidget()?.type;
+}
