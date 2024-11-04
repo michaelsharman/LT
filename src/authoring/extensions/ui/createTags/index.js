9f30f6b3ed2f08e9f4a0ea99f964520039f5f4e2
Michael Sharman <sharmo@gmail.com>
2024-11-05 09:20:13 +1100

    Changed error message from syntax to format.

--- src/authoring/extensions/ui/createTags/index.js
+++ src/authoring/extensions/ui/createTags/index.js
@@ -81,7 +81,7 @@ function showCreateTagsUI(elNoSuggestions) {
                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
-                <span class="lt__errorMessage">Invalid syntax. Use [type]:[name]</span>
+                <span class="lt__errorMessage">Invalid format. Use [type]:[name]</span>
             </span>
             <button type="button" id="lt__createTagsBtn" class="lrn-btn lrn-btn-primary lds-btn lds-btn-primary lds-btn-sm">Create</button>
         </div>
