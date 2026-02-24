8b39bb38354c03699eed24d616e89ef50351d06f
Michael Sharman <sharmo@gmail.com>
2026-02-25 09:59:32 +1100

    Remove dist from repository, removed CDN option

--- dist/isSymbol-BPiEP3ls.js
+++
@@ -1,8 +0,0 @@
-import { i as b, b as s } from "./isObjectLike-B0-0sm4Y.js";
-var t = "[object Symbol]";
-function e(o) {
-  return typeof o == "symbol" || b(o) && s(o) == t;
-}
-export {
-  e as i
-};
