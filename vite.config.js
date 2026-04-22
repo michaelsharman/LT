78b2c738423382d444578c6de6d05537b76a6348
Michael Sharman <sharmo@gmail.com>
2026-04-22 13:09:52 +1000

    Updated all packages excl Uppy.

--- vite.config.js
+++ vite.config.js
@@ -78,7 +78,7 @@ export default defineConfig(({ command }) => {
                 // fileName: (format, entryName) => `${entryName}.js`,
             },
 
-            rollupOptions: {
+            rolldownOptions: {
                 treeshake: true,
             },
         },
